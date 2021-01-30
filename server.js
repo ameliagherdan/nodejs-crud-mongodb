666c6e18aebbb23028d6f80e2fc085148b6095d2
ameliagherdan <ameliagherdan@gmail.com>
2021-01-30 19:53:07 +0200

    adding validations name and email

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express');
 const path = require('path');
 const exphbs = require ('express-handlebars');
 const employeeController = require('./controllers/employeeController');
+const bodyparser = require('body-parser');
 
 var app = express();
+app.use(bodyparser.urlencoded({
+    extended:true
+}));
+app.use(bodyparser.json());
 app.set('views', path.join(__dirname,'/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
